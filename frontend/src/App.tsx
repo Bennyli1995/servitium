30b934425f35f2a576a76d8c6221921c93d7e7a6
Benny Li <59155001+Bennyli1995@users.noreply.github.com>
2024-01-20 14:25:48 -0800

    set up worker pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
 import ProfilePage from "./pages/ProfilePage";
 import SettingsPage from "./pages/SettingsPage";
+import CategoryPage from "./pages/CategoryPage";
+import WorkerDetailsPage from "./pages/WorkerDetailsPage";
 
 const App: React.FC = () => {
   return (
-    <Router>
+    <BrowserRouter>
       <div className="flex flex-col h-screen">
         <Header /> {/* This will always be displayed */}
         <main className="flex-grow">
@@ -17,12 +19,13 @@ const App: React.FC = () => {
             <Route path="/" element={<HomePage />} />
             <Route path="/profile" element={<ProfilePage />} />
             <Route path="/settings" element={<SettingsPage />} />
-            {/* Add more routes as needed */}
+            <Route path="/category/:categoryLabel" element={<CategoryPage />} />
+            <Route path="/worker/:workerId" element={<WorkerDetailsPage />} />
           </Routes>
         </main>
         <Footer />
       </div>
-    </Router>
+    </BrowserRouter>
   );
 };
 
