c6ead5851242bf8fc5997c0f71a8a14c3c935619
kaigg96 <kaigg@student.ubc.ca>
2024-01-20 17:15:10 -0800

    finalized merge

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -77,31 +77,6 @@ app.post('/recommend', async (req, res) => {
   }
 });
 
-// POST request for recommendation
-app.post('/recommend', async (req, res) => {
-  try {
-    let workers = await WorkerModel.find().exec();
-    
-    const { message, trade } = req.body;
-    workers = workers.filter(worker => worker.trade === trade);
-
-    const gpt_prompt = "Request: " + message +
-    " Choose the top three workers from the list, returning only their worker_ids in the content of your message. " +
-    "Worker List: " + JSON.stringify(workers) + 
-    "You will give the result of my query in the following format: " + 
-    "Result: {cheapest_id: 1, second_cheapest_id: 2, third_cheapest_id: 3}";
-
-    const workerRecommendation = await getWorkerRecommendation(gpt_prompt);
-
-    res.json({ recommendation: workerRecommendation });
-    console.log(workerRecommendation.message.content);
-  } catch (error: any) {
-    console.error('Error:', error.message);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
-
-
 // POST endpoint to add a review
 app.post("/add_review", async (req, res) => {
   try {
